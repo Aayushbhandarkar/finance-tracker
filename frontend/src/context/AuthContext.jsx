4529add58ceea06778e16578d47157464a2811ce
Ayush Bhandarkar <163156376+Aayushbhandarkar@users.noreply.github.com>
2025-09-11 11:11:30 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', {
+      const response = await axios.post('https://finance-tracker-backend-afpg.onrender.com', {
         email,
         password
       });
