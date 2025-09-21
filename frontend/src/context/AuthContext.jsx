f38ffb6822a66b39f1c0482463d1d5b6b88e6aba
Ayush Bhandarkar <ayushbhandarkar7@gmail.com>
2025-09-21 09:38:50 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,6 @@ import React, { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 
 const AuthContext = createContext();
-
 export const useAuth = () => {
   return useContext(AuthContext);
 };
